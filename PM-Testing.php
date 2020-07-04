b7eccd0a334fd98d2568f0d4009123971ea23a49
mspipat <ppatena24@gmail.com>
2020-07-04 11:48:05 +0800

    empty PM

--- PM-Testing.php
+++ PM-Testing.php
@@ -1,81 +1,56 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<?php
-		include 'src/link.php';
-	?>
-	<title> SD Data Wrangling Tool</title>
-</head>
-<body style="background-image: url('img/computer.jpg');background-size: cover;">
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-lg-12">
-				<h4 class="card-title text-center font-weight-bolder mt-1 text-uppercase"> Upload SD Tool</h4>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-8">
-			<div class="card-body white">
-				<form action="" method="post" enctype="multipart/form-data" id="fileUploadForm" class="">
-	   		<div class="input-group text">
-							<div class="input-group-prepend">
-								<span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-							</div>
-							<div class="custom-file">
-								<input type="file" class="custom-file-input" id="dataExcel" accept="application/vnd.ms-excel,.xlsx" name="file">
-								<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<button type="submit" class="btn btn-md mt-2 btn-primary text-center" id="btnSubmit">Submit</button>
-							</div>
-						</div>
-				</form>
-				<div class="row text-center" id="reportSection" style="display:none;">
-					<div class="col-lg-12">
-							<h5>Successfully Generated SD Tool!</h5>
-					</div>
-					<div class="col-lg-4"></div>
-					<div class="col-lg-4">
-						<i class="fa fa-download fa-4x" aria-hidden="true"></i><br>
-					<a id="linkReport">Download Report</a>
-					<button class="btn btn-md btn-danger" id="btnNew">Upload New File</button>
-					</div>
-					<!-- <a id="linkReport" style="display: none;">Download Report</a> -->
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 <?php
-	include 'src/script.php';
-?>
-<?php`
 	include 'db/conn.php';
 	require 'plugins/phpspreadsheet/vendor/autoload.php';
 	use PhpOffice\PhpSpreadsheet\Spreadsheet;
 	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-		if(isset($_FILES['file'])){
-			// variables 
-			$ipAdd = $_SERVER['REMOTE_ADDR'];
-			$date = date('Y-m-d h i s');
-			$uploadName = $_FILES['file']['name'];
-			$fileType = $_FILES['file']['type'];
-			// Check File Type
-			if($fileType == 'application/vnd.ms-excel')
-			{	
-				$newFileName = 'PM-'.$date.' '.$ipAdd.'-'.$uploadName;
-				// Uploaded File
-				if( move_uploaded_file($_FILES["file"]["tmp_name"],"process/uploads/PM Data/".$newFileName))
-				{
-					$dateStart = date('Y-m-d h:i:s');
-					$sqlInsertRec = "INSERT INTO `system_activity`(`ipAdd`, `activity`, `datetime`) VALUES ('$ipAdd','Upload PM Data','$dateStart')";
-					$query = $conn_db->query($sqlInsertRec);
-					// END Record System Activity 
-					$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
-					$uploaded_sheet = $reader->load("process/uploads/PM Data/".$newFileName);
-				}
+
+	$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+	$uploaded_sheet = $reader->load("process/uploads/PM Data/testPM.xls");
+
+	// ARRAYS 
+	$cc = 'starting';
+	$offRow = 2;
+	
+	$ipAdd = $_SERVER['REMOTE_ADDR'];
+	$tableName = 'pmTemp_'.$ipAdd;
+	$sqlCreateTbl = "CREATE TABLE `$tableName` ( `listId` INT NOT NULL AUTO_INCREMENT , `connNo` INT(11) NOT NULL , `partNo` INT(11) NOT NULL , `partClass` VARCHAR(11) NOT NULL , `partsName` VARCHAR(255) NOT NULL , `wireLenght` INT(11) NOT NULL , `method` VARCHAR(255) NOT NULL , PRIMARY KEY (`listId`));";
+	$query = $conn_db->query($sqlCreateTbl);
+
+	do {
+		$cc = $uploaded_sheet->getActiveSheet()->getCell('A'.$offRow)->getValue();
+
+		echo 'Conn No: '.$connNo = $uploaded_sheet->getActiveSheet()->getCell('A'.$offRow)->getValue();
+		echo ' Parts Class: '.$partsClass = $uploaded_sheet->getActiveSheet()->getCell('D'.$offRow)->getValue();
+		echo ' Parts Name: '.$partsName = $uploaded_sheet->getActiveSheet()->getCell('F'.$offRow)->getValue();
+		echo ' Parts Number: '.$partsNumber = $uploaded_sheet->getActiveSheet()->getCell('C'.$offRow)->getValue();
+		echo ' Length: '.$length = $uploaded_sheet->getActiveSheet()->getCell('G'.$offRow)->getValue(); 
+		echo ' Method: '.$method = $uploaded_sheet->getActiveSheet()->getCell('H'.$offRow)->getValue();
+		if($length == ''){
+			if((strstr($partsName, 'STU')) || (strstr($partsName, 'MARUBO'))|| (strstr($partsName, 'STG'))){
+				$partsNameData = explode("X",$partsName);
+				$lenghtData = $partsNameData[2];
+				$length = preg_replace("/[^0-9]/", "", $lenghtData);
 			}
 		}
+		echo "<br>";
+		// $sqlInsert = "INSERT INTO `$tableName`(`connNo`, `partNo`, `partClass`, `partsName`, `wireLenght`, `method`) 
+		// VALUES ('$connNo','$partsNumber','$partsClass','$partsName','$length','$method')";
+		// $query = $conn_db->query($sqlInsert);
+		$offRow++;
+
+	} while ($cc != NULL);
+	$sqlSelectSameCount = "SELECT COUNT(listId) AS num, connNo,  FROM `$tableName` GROUP BY connNo";
+	$queryCount = $conn_db->query($sqlSelectSameCount);
+	while ($data = $queryCount->fetch_assoc()) {
+		$num = $data['num'];
+		if($num >= 2){
+			$sqlGetData = "SELECT * FROM `$tableName` WHERE connNo";
+			$queryData = $conn_db->query($sqlGetData);
+
+		}
+	}
+	print_r($sameConnNo);
+	// $sqlDeleteTbl = "DROP TABLE `$tableName`";
+	// $query1 = $conn_db->query($sqlDeleteTbl);
+	 
 ?>
--- generatePM.php
+++ generatePM.php
@@ -7,75 +7,7 @@
 	<title> SD Data Wrangling Tool</title>
 </head>
 <body style="background-image: url('img/computer.jpg');background-size: cover;">
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-lg-12">
-				<h4 class="card-title text-center font-weight-bolder mt-1 text-uppercase"> Upload SD Tool</h4>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-8">
-			<div class="card-body white">
-				<form action="" method="post" enctype="multipart/form-data" id="fileUploadForm" class="">
-	   		<div class="input-group text">
-							<div class="input-group-prepend">
-								<span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-							</div>
-							<div class="custom-file">
-								<input type="file" class="custom-file-input" id="dataExcel" accept="application/vnd.ms-excel,.xlsx" name="file">
-								<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<button type="submit" class="btn btn-md mt-2 btn-primary text-center" id="btnSubmit">Submit</button>
-							</div>
-						</div>
-				</form>
-				<div class="row text-center" id="reportSection" style="display:none;">
-					<div class="col-lg-12">
-							<h5>Successfully Generated SD Tool!</h5>
-					</div>
-					<div class="col-lg-4"></div>
-					<div class="col-lg-4">
-						<i class="fa fa-download fa-4x" aria-hidden="true"></i><br>
-					<a id="linkReport">Download Report</a>
-					<button class="btn btn-md btn-danger" id="btnNew">Upload New File</button>
-					</div>
-					<!-- <a id="linkReport" style="display: none;">Download Report</a> -->
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+
 <?php
 	include 'src/script.php';
-?>
-<?php`
-	include 'db/conn.php';
-	require 'plugins/phpspreadsheet/vendor/autoload.php';
-	use PhpOffice\PhpSpreadsheet\Spreadsheet;
-	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-		if(isset($_FILES['file'])){
-			// variables 
-			$ipAdd = $_SERVER['REMOTE_ADDR'];
-			$date = date('Y-m-d h i s');
-			$uploadName = $_FILES['file']['name'];
-			$fileType = $_FILES['file']['type'];
-			// Check File Type
-			if($fileType == 'application/vnd.ms-excel')
-			{	
-				$newFileName = 'PM-'.$date.' '.$ipAdd.'-'.$uploadName;
-				// Uploaded File
-				if( move_uploaded_file($_FILES["file"]["tmp_name"],"process/uploads/PM Data/".$newFileName))
-				{
-					$dateStart = date('Y-m-d h:i:s');
-					$sqlInsertRec = "INSERT INTO `system_activity`(`ipAdd`, `activity`, `datetime`) VALUES ('$ipAdd','Upload PM Data','$dateStart')";
-					$query = $conn_db->query($sqlInsertRec);
-					// END Record System Activity 
-					$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
-					$uploaded_sheet = $reader->load("process/uploads/PM Data/".$newFileName);
-				}
-			}
-		}
 ?>
